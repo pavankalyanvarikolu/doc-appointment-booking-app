d7f3dd5087a53b6db63fc43fce5fb5b3dcbc4145
pavan kalyan <p.kalyanvarikolu@gmail.com>
2024-10-13 16:35:55 +0100

    adding all the modified files and newly added files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{port:5173}
-})
+export default defineConfig(({ command, mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      host: true,
+      watch: {
+        usePolling: true,
+      },
+    },
+    define: {
+      VITE_BACKEND_URL: JSON.stringify(env.VITE_BACKEND_URL),
+    },
+  };
+});
